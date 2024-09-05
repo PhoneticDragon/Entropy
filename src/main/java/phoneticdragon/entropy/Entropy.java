ddda3f26d62f2fde92128bca9fd07594491014c4
PhoneticDragon <herobrinegaming843@gmail.com>
2024-09-05 18:02:10 +1200

    Created Mod

---
+++ src/main/java/phoneticdragon/entropy/Entropy.java
@@ -0,0 +1,10 @@
+package phoneticdragon.entropy;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Entropy implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/phoneticdragon/entropy/EntropyClient.java
@@ -0,0 +1,10 @@
+package phoneticdragon.entropy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EntropyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
